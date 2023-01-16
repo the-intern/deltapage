d082d2d65cdc78da7e438adab7b83dcfa089bd76
the-intern <ostclasses@gmail.com>
2023-01-16 09:56:07 -0600

    basic changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,10 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Deltapage`,
+    description: `A searchable page`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://deltapage.vercel.app/`,
   },
   plugins: [
     `gatsby-plugin-image`,
